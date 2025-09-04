4f5ae45f708e23bd7d4de9138f331576c793e7e0
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-09-04 15:54:59 -0700

    feat: add a page to address unknown routes, hook up catch all route in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import LoginScreen from "./pages/LoginScreen";
 import GamePage from "./pages/GamePage";
+import UnkownRoute from "./pages/UnknownRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
       <Route path="/login" element={<LoginScreen />} />
-      <Route path="/game" element = {<GamePage />} />
+      <Route path="/game" element={<GamePage />} />
+      <Route path="*" element={<UnkownRoute />} />
     </Routes>
   );
 }
