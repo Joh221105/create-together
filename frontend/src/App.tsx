4724ba8563d0081643a0d735b2620612d94e1d2b
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2025-09-04 17:04:53 -0700

    feat: create and route temporary room page for guest rooms

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import GamePage from "./pages/GamePage";
 import UnkownRoute from "./pages/UnknownRoute";
 import SignUpPage from "./pages/SignUpPage";
 import GuestRoomSetup from "./pages/GuestRoomSetup";
+import RoomPage from "./pages/RoomPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Route path="/game" element={<GamePage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/guest-room" element={<GuestRoomSetup />} />
+      <Route path ="/room/:roomId" element={<RoomPage />} />
       <Route path="*" element={<UnkownRoute />} />
     </Routes>
   );
---
+++ frontend/src/pages/RoomPage.tsx
@@ -0,0 +1,9 @@
+const RoomPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-100">
+      <h1 className="text-3xl font-bold">Welcome to the Room 🎨</h1>
+    </div>
+  );
+};
+
+export default RoomPage;
